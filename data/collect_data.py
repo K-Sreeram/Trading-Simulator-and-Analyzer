a74ca378a4ab8a1b947c9ef9886442af32f19925
K-Sreeram <ssriram693@gmail.com>
2025-02-19 07:36:27 +0000

    collect_data.py file is updated to take input

--- data/collect_data.py
+++ data/collect_data.py
@@ -1,10 +1,11 @@
 from jugaad_data.nse import stock_df
 from datetime import datetime, timedelta
 
-to_date = datetime.today().date()  
-from_date = to_date - timedelta(days=7)  
+to_date = datetime.today().date()
+company_name=input("Enter stock name:")  
+from_date = to_date - timedelta(days=int(input("enter days:")))  
 # Get historical stock data for RELIANCE
-df = stock_df(symbol="RELIANCE", from_date=from_date, to_date=to_date, series="EQ")
+df = stock_df(symbol=company_name, from_date=from_date, to_date=to_date, series="EQ")
 
 df.to_csv("stock_data.csv", index=False)
 
